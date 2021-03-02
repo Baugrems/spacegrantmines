cc3fd12de03c5bbee7027b614f25437c0a9bef1c
nighthawk30 <nathan4taylor@gmail.com>
2021-03-02 16:55:53 -0700

    The most skeleton of skeletons, we need to make the wheels turn

---
+++ algorithms/maincontrol.py
@@ -0,0 +1,34 @@
+#import basically everything
+#do we need to have event checker things?
+
+def main():
+    #setup
+    #run loop (threading?)
+
+
+    #The process
+    #get all inputs
+    #Get beacon direction
+    #get the map/update the map
+    #if way ahead is clear, turn towards beacon and move forward
+    #otherwise, update the map with new obstacle and path find a way around the obstacle to the beacon direction
+
+
+
+def turn_wheel(int wheel, float angle):
+
+def rotate_wheel(int wheel, float distance)
+
+def turn_robot(float angle):
+
+def move_robot(float distance):
+
+def get_beacon_angle():
+
+def get_heading():
+
+def get_beacon_direction():
+
+def get_laser_dist():
+
+def update_map():#we want to be near the center of the map
