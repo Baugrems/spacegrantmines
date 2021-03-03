6d65865a2f8d427199f672e39874100b0e8b8c77
nighthawk30 <nathan4taylor@gmail.com>
2021-03-02 18:02:13 -0700

    I have no idea how to import

--- algorithms/maincontrol.py
+++ algorithms/maincontrol.py
@@ -1,9 +1,10 @@
-from spacegrantmines.rotateXRevolutions.RotateTest import WheelController as wc
+#import .spacegrantmines.rotateXRevolutions.RotateTest.import_test as wc
+#from ../ spacegrantmines.rotateXRevolutionsTest.RotateTest.import_test as wc
 #do we need to have event checker things?
 
 def main():
-    test = wc.Wheel(0x04, smbus.SMBus(1), setDirection=True)#calibration test
-
+    #test = wc.Wheel(0x04, smbus.SMBus(1), setDirection=True)#calibration test
+    wc.print_stuff()
     #setup
     #run loop (threading?)
     
@@ -16,22 +17,31 @@ def main():
 
 
 
-def turn_wheel(int wheel, float angle):
-
-def rotate_wheel(int wheel, float distance):
-
-def turn_robot(float angle):
-
-def move_robot(float distance):
-
+def turn_wheel(wheel, angle):
+    #do nothing
+    print("hello")
+def rotate_wheel(wheel, distance):
+    #do nothing
+    print("workkkkk")
+def turn_robot(angle):
+    #do nothing
+    print("workkkkk")
+def move_robot(distance):
+    #do nothing
+    print("workkkkk")
 def get_beacon_angle():
-
+    #do nothing
+    print("workkkkk")
 def get_heading():
-
+    #do nothing
+    print("workkkkk")
 def get_beacon_direction():
-
+    #do nothing
+    print("workkkkk")
 def get_laser_dist():
-
+    #do nothing
+    print("workkkkk")
 def update_map():#we want to be near the center of the map
-
+    #do nothing
+    print("workkkkk")
 main()
